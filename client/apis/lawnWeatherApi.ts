14d59990da6f755c90668e6a603b31bf40be4ec1
cody-ilg <143582504+cody-ilg@users.noreply.github.com>
2023-11-14 17:08:48 +1300

    tidyArr fn working

--- client/apis/lawnWeatherApi.ts
+++ client/apis/lawnWeatherApi.ts
@@ -2,24 +2,24 @@ import request from 'superagent'
 
 export async function getWormWeather() {
   const httpRequestObject = await request.get(
-    'http://localhost:5173/api/v1/soildata'
+    'http://localhost:5173/api/v1/soildata',
   )
 
   function tidyArr(arr) {
-    let indexestoKeep = [11, 35, 59]
-    
+    const noonHours = arr.filter((element, index) => {
+      return index === 11 || index === 35 || index === 59
+    })
+    return noonHours
   }
 
-
   const weatherArr = []
-  const soil0_1 = httpRequestObject.body.hourly.soil_moisture_0_to_1cm
-  const soil1_3 = httpRequestObject.body.hourly.soil_moisture_1_to_3cm
-  const soil3_9 = httpRequestObject.body.hourly.soil_moisture_3_to_9cm
-  const soil9_27 = httpRequestObject.body.hourly.soil_moisture_9_to_27cm
-  const soil27_81 = httpRequestObject.body.hourly.soil_moisture_27_to_81cm
+  const soil0_1 = tidyArr(httpRequestObject.body.hourly.soil_moisture_0_to_1cm)
+  const soil1_3 = tidyArr(httpRequestObject.body.hourly.soil_moisture_1_to_3cm)
+  const soil3_9 = tidyArr(httpRequestObject.body.hourly.soil_moisture_3_to_9cm)
+  const soil9_27 = tidyArr(httpRequestObject.body.hourly.soil_moisture_9_to_27cm)
+  const soil27_81 = tidyArr(httpRequestObject.body.hourly.soil_moisture_27_to_81cm)
 
-  const hourslyArr = httpRequestObject.body.hourly
 
-  console.log(soil1_3)
+
   return httpRequestObject.body
 }
